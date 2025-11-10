1df87789c6659b2ba9f8c16b33e7071e79ce7e9b
ryanakbar20 <ryanakbar0987@gmail.com>
2025-11-10 23:19:43 +0700

    feat: enhance body email template

--- resources/views/emails/bodyEmailCompany.blade.php
+++ resources/views/emails/bodyEmailCompany.blade.php
@@ -1,105 +1,107 @@
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="512" align="center" class="m_-6349447554292269623mercado-container" style="margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto;width:512px;max-width:512px;background-color:#ffffff;padding:0px">
-<tbody>
-<tr>
-<td style="padding:24px;text-align:center">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="min-width:100%">
-<tbody>
-<tr>
-<td align="left" valign="middle">
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td style="padding:0px;padding-bottom:0px!important">
-<div>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td style="border-width:0;border-bottom-width:1px;border-style:solid;border-color:#e6e6e6;padding:24px">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td style="width:72px;" width="72" align="center">
-</td>
-</tr>
-<tr>
-<td>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td align="center" style="font-size:16px;font-weight:600;line-height:1.25;color:#282828;padding-top:12px;">{{ $name }}</td>
-</tr>
-<tr>
-<td align="center" style="font-size:13px;font-weight:600;line-height:1.25;color:#282828;padding-top:12px;">Company : {{ $company }}</td>
-</tr>
-<tr>
-<td align="center" style="font-size:12px;font-weight:400;line-height:1.25;color:#282828;padding-top:4px;">Email : {{ $email }}</td>
-</tr>
-<tr>
-<td align="center" style="font-size:12px;font-weight:400;line-height:1.25;color:#282828;padding-top:4px;">Number : {{ $phone }}</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td align="center" style="padding-top:12px;font-size:13px;font-weight:400;line-height:1.25;color:#282828;">({{ $option }})</td>
-</tr>
-<tr>
-<td colspan="1" style="padding-top:4px;font-size:16px;line-height:1.25;color:#282828;text-align: center;">
-<span>{{ $question }}</span>
-</td>
-</tr>
-<tr>
-<td colspan="1" style="padding-top:24px">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%">
-<tbody>
-<tr>
-<td valign="middle" align="middle">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="border-collapse:separate;width:100%!important">
-<tbody>
-<tr>
-<td style="height:min-content;border-radius:24px;padding-top:12px;padding-bottom:12px;padding-left:24px;padding-right:24px;text-align:center;font-size:16px;font-weight:600;text-decoration-line:none;background-color:rgba(0,0,0,0);color:#0a66c2;border-width:1px;border-style:solid;border-color:#0a66c2;line-height:1.25;min-height:auto!important">
-<span style="color:#0a66c2;text-decoration-line:none"> Contact {{ $name }} </span>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr><td style="height:16px" height="16"></td></tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tr>
-</tbody>
-</table>
-</div>
-</td>
-</tr>
-</tbody>
+<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0"
+    width="512" align="center"
+    style="margin:0 auto;width:512px;max-width:512px;background-color:#ffffff;padding:0;">
+    <tbody>
+        <tr>
+            <td style="padding:24px;text-align:center">
+                <table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%"
+                    style="min-width:100%">
+                    <tbody>
+                        <tr>
+                            <td align="left" valign="middle">
+                                {{-- Optional header image/logo bisa kamu tambahkan di sini --}}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td style="padding:0;">
+                <div>
+                    <table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
+                        <tbody>
+                            <tr>
+                                <td
+                                    style="border-bottom:1px solid #e6e6e6;padding:24px;font-family:Arial,Helvetica,sans-serif;">
+                                    <table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0"
+                                        width="100%">
+                                        <tbody>
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:16px;font-weight:600;line-height:1.5;color:#282828;padding-top:8px;">
+                                                    {{ $name }} 様より
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:14px;font-weight:500;line-height:1.5;color:#282828;padding-top:8px;">
+                                                    御社名 / Company Name : {{ $company }}
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:14px;font-weight:500;line-height:1.5;color:#282828;padding-top:4px;">
+                                                    メールアドレス / Email Address : {{ $email }}
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:14px;font-weight:500;line-height:1.5;color:#282828;padding-top:4px;">
+                                                    電話番号 / Contact Number : {{ $phone }}
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:15px;font-weight:600;line-height:1.5;color:#282828;padding-top:16px;">
+                                                    お問い合わせ内容:
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:14px;font-weight:400;line-height:1.6;color:#282828;padding-top:4px;">
+                                                    {{ $question }}
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="padding-top:24px;text-align:center;">
+                                                    <table role="presentation" border="0" cellspacing="0"
+                                                        cellpadding="0" align="center"
+                                                        style="margin:0 auto;border-collapse:separate;">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td
+                                                                    style="border-radius:24px;padding:12px 24px;text-align:center;font-size:16px;font-weight:600;background-color:#fff;color:#0a66c2;border:1px solid #0a66c2;line-height:1.25;">
+                                                                    <a href="mailto:{{ $email }}"
+                                                                        style="color:#0a66c2;text-decoration:none;">
+                                                                        Contact {{ $name }}
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="height:16px;" height="16"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </tbody>
 </table>
--- resources/views/emails/bodyEmailSender.blade.php
+++ resources/views/emails/bodyEmailSender.blade.php
@@ -1,101 +1,80 @@
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="512" align="center" class="m_-6349447554292269623mercado-container" style="margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto;width:512px;max-width:512px;background-color:#ffffff;padding:0px">
-<tbody>
-<tr>
-<td style="padding:24px;text-align:center">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="min-width:100%">
-<tbody>
-<tr>
-<td align="left" valign="middle">
-<a href="https://www.olldesign.jp/">
-<img alt="ol-design" src="	https://olldesign.jp/assets/images/OLL_DESIGN.png" style="outline:none;text-decoration:none;display:block;width:100%;height:100%;object-fit: cover;"
-</a>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td style="padding:0px;padding-bottom:0px!important">
-<div>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td style="border-width:0;border-bottom-width:1px;border-style:solid;border-color:#e6e6e6;padding:24px">
-<a href="https://www.olldesign.jp/" style="color:#1e9e05;display:inline-block;text-decoration:none;width:100%" target="_blank">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td>
-<a href="https://www.olldesign.jp/" style="color:#1e9e05;text-decoration:none;vertical-align:top;text-decoration-line:none;width:100%!important;display:block!important" target="_blank">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td>
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%">
-<tbody>
-<tr>
-<td align="center" style="font-size:16px;font-weight:600;line-height:1.25;color:#282828;padding-top:12px;">Hi {{ $name }}, Thank you for contacting us we will reach you out shortly to your email : {{ $email }}</td>
-</tr>
-<tr>
-<td align="center" style="font-size:14px;font-weight:600;line-height:1.25;color:#282828;padding-top:12px;">Question :</td>
-</tr>
-<tr>
-<td align="center" style="font-size:14px;font-weight:400;line-height:1.25;color:#282828;padding-top:4px;">{{ $question }}</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</a>
-</td>
-</tr>
-<tr>
-<td colspan="1" style="padding-top:24px">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%">
-<tbody>
-<tr>
-<td valign="middle" align="middle">
-<a href="https://www.olldesign.jp/" style="color:#1e9e05;text-decoration:none;vertical-align:top;text-decoration-line:none;width:100%!important;display:block!important" target="_blank">
-<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="100%" style="border-collapse:separate;width:100%!important">
-<tbody>
-<tr>
-<td style="height:min-content;border-radius:24px;padding-top:12px;padding-bottom:12px;padding-left:24px;padding-right:24px;text-align:center;font-size:16px;font-weight:600;text-decoration-line:none;background-color:rgba(0,0,0,0);color:#1e9e05;border-width:1px;border-style:solid;border-color:#1e9e05;line-height:1.25;min-height:auto!important">
-<a href="https://www.olldesign.jp/" style="color:#1e9e05;text-decoration:none;vertical-align:top;text-decoration-line:none;width:100%!important;display:block!important" target="_blank">
-<span style="color:#1e9e05;text-decoration-line:none">Back to Oll Design Website</span>
-</a>
-</td>
-</tr>
-</tbody>
-</table>
-</a>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr><td style="height:16px" height="16"></td></tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</a>
-</td>
-</tr>
-</tr>
-</tbody>
-</table>
-</div>
-</td>
-</tr>
-</tbody>
+<table role="presentation" valign="top" border="0" cellspacing="0" cellpadding="0" width="512" align="center" style="margin:0 auto;width:512px;max-width:512px;background-color:#ffffff;padding:0;">
+  <tbody>
+    <tr>
+      <td style="padding:24px;text-align:center">
+        <table role="presentation" border="0" cellspacing="0" cellpadding="0" width="100%">
+          <tbody>
+            <tr>
+              <td align="left" valign="middle">
+                <a href="https://www.olldesign.jp/">
+                  <img alt="OLLDESIGN" src="https://olldesign.jp/assets/images/OLL_DESIGN.png" style="outline:none;text-decoration:none;display:block;width:100%;height:100%;object-fit:cover;">
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+
+    <tr>
+      <td style="border-bottom:1px solid #e6e6e6;padding:24px;">
+        <table role="presentation" border="0" cellspacing="0" cellpadding="0" width="100%">
+          <tbody>
+            <tr>
+              <td align="center" style="font-size:18px;font-weight:700;color:#1e9e05;padding-bottom:12px;">
+                OLLDESIGN株式会社
+              </td>
+            </tr>
+
+            <tr>
+              <td align="center" style="font-size:16px;font-weight:500;line-height:1.6;color:#282828;">
+                Hi {{ $name }},
+                <br>
+                Thank you for contacting us.<br>
+                We truly appreciate your message and will reach out to you shortly at <strong>{{ $email }}</strong>.
+              </td>
+            </tr>
+
+            <tr>
+              <td align="center" style="font-size:15px;font-weight:400;line-height:1.6;color:#282828;padding-top:16px;">
+                この度は <strong>OLLDESIGN株式会社</strong> にお問い合わせいただき、誠にありがとうございます。<br>
+                内容を確認のうえ、近日中に <strong>{{ $email }}</strong> 宛てにご連絡いたします。
+              </td>
+            </tr>
+
+            <tr>
+              <td align="center" style="font-size:16px;font-weight:600;line-height:1.5;color:#282828;padding-top:24px;">
+                Your Message / お問い合わせ内容：
+              </td>
+            </tr>
+
+            <tr>
+              <td align="center" style="font-size:14px;font-weight:400;line-height:1.5;color:#282828;padding-top:8px;">
+                {{ $question }}
+              </td>
+            </tr>
+
+            <tr>
+              <td align="center" style="padding-top:32px;">
+                <a href="https://olldesign.jp/" target="_blank" style="text-decoration:none;">
+                  <table role="presentation" border="0" cellspacing="0" cellpadding="0" width="100%" style="border-collapse:separate;">
+                    <tbody>
+                      <tr>
+                        <td style="border-radius:24px;padding:12px 24px;text-align:center;font-size:16px;font-weight:600;color:#1e9e05;border:1px solid #1e9e05;">
+                          Back to OLLDESIGN website | OLLDESIGNのHPに戻る
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </a>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </td>
+    </tr>
+
+    <tr><td style="height:16px" height="16"></td></tr>
+  </tbody>
 </table>
